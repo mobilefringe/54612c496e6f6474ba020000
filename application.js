1ed326a1951bbd259d5ba623224d3ede5e66698c
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-23 13:57:23 -0400

    Londonderry 1.1  /application.js

--- application.js
+++ application.js
@@ -22,3 +22,31 @@ function renderHomeHours(container, template){
     });
     $(container).html(item_rendered.join(''));
 }
+
+
+
+function get_day(id){
+    switch(id) {
+        case 0:
+            return ("Sunday");
+            break;
+        case 1:
+            return ("Monday");
+            break;
+        case 2:
+            return ("Tuesday");
+            break;
+        case 3:
+            return ("Wednesday");
+            break;
+        case 4:
+            return ("Thursday");
+            break;
+        case 5:
+            return ("Friday");
+            break;
+        case 6:
+            return ("Saturday");
+            break;
+    }
+}
