b70a3b592fc74e220af4a06e8243604f07c1f831
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-08-24 11:09:35 -0400

    Londonderry 1.1  /application.js

--- application.js
+++ application.js
@@ -165,6 +165,38 @@ function renderPromotionDetails(container, template, collection){
     $(container).html(item_rendered.join(''));
 }
 
+
+function renderJobs(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        if(val.jobable_type == "Store"){
+            val.store_name = getStoreDetailsByID(val.jobable_id).name;
+            val.store_slug = getStoreDetailsByID(val.jobable_id).slug;
+        }
+        else{
+            val.store_name = "Dixie Outlet";
+        }
+        var show_date = moment(val.show_on_web_date);
+        var start = moment(val.start_date).tz(getPropertyTimeZone());
+        var end = moment(val.end_date).tz(getPropertyTimeZone());
+        if (start.format("DMY") == end.format("DMY")){
+            val.dates = start.format("MMM D")
+        }
+        else{
+            val.dates = start.format("MMM D") + " - " + end.format("MMM D")
+        }
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
+
+
 function in_my_time_zone(hour, format){
     return hour.tz(getPropertyTimeZone()).format(format)
 }
