6ff7f2dc2dd0e1013b876c26380809b9ba9a4f0d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-12-05 12:00:39 -0500

    Londonderry 1.2  /application.js

--- application.js
+++ application.js
@@ -258,14 +258,3 @@ function get_day(id){
             break;
     }
 }
-function renderGeneral(container, template, collection){
-    var item_list = [];
-    var item_rendered = [];
-    var template_html = $(template).html();
-    Mustache.parse(template_html); 
-    $.each( collection , function( key, val ) {
-        var repo_rendered = Mustache.render(template_html,val);
-        item_rendered.push(repo_rendered);
-    });
-    $(container).html(item_rendered.join(''));
-}
