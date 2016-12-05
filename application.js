94db9deba94b126efe7cdf0f1cc87afab9ed95d6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-12-05 11:57:09 -0500

    Londonderry 1.2  /application.js

---
+++ application.js
@@ -0,0 +1,271 @@
+function renderHomeHours(container, template){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    var collection = getTodaysHours();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    item_list.push(collection);    
+    $.each( item_list , function( key, val ) {
+        val.day = get_day(val.day_of_week);
+        var d = moment().tz(getPropertyTimeZone());
+        val.month = d.format("MMM")
+        val.weekday = d.format("dd")
+        if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
+            var open_time = in_my_time_zone(moment(val.open_time), "h:mm A");
+            var close_time = in_my_time_zone(moment(val.close_time), "h:mm A");
+            val.h = open_time + " - " + close_time;
+        } else {
+            val.h = "Closed";
+        }
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
+function renderHours(container, template, collection, type){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    if (type == "reg_hours") {
+        $.each( collection , function( key, val ) {
+            if (!val.store_id && val.is_holiday == false) {
+                switch(val.day_of_week) {
+                    case 0:
+                        val.day = "Sunday";
+                        break;
+                    case 1:
+                        val.day = "Monday";
+                        break;
+                    case 2:
+                        val.day = "Tuesday";
+                        break;
+                    case 3:
+                        val.day = "Wednesday";
+                        break;
+                    case 4:
+                        val.day = "Thursday";
+                        break;
+                    case 5:
+                        val.day = "Friday";
+                        break;
+                    case 6:
+                        val.day = "Saturday";
+                        break;
+                }
+                if (val.open_time && val.close_time && val.is_closed == false){
+                    var open_time = in_my_time_zone(moment(val.open_time), "h:mm A");
+                    var close_time = in_my_time_zone(moment(val.close_time), "h:mm A");
+                    val.h = open_time + " - " + close_time;
+                } else {
+                    "Closed";
+                }
+                
+                item_list.push(val);
+            }
+        });
+        collection = [];
+        collection = item_list;
+    }
+    
+    if (type == "holiday_hours") {
+        $.each( collection , function( key, val ) {
+            if (!val.store_id && val.is_holiday == true) {
+                holiday = moment(val.holiday_date);
+                val.formatted_date = in_my_time_zone(holiday, "ddd MMM DD YYYY");
+                if (val.open_time && val.close_time && val.is_closed == false){
+                    var open_time = in_my_time_zone(moment(val.open_time), "h:mm A");
+                    var close_time = in_my_time_zone(moment(val.close_time), "h:mm A");
+                    val.h = open_time + " - " + close_time;   
+                } else {
+                    val.h = "Closed";
+                }
+                item_list.push(val);
+            }
+        });
+        collection = [];
+        collection = item_list;
+    }
+    
+    $.each( collection , function( key, val ) {
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+
+    });
+    
+    $(container).show();
+    $(container).html(item_rendered.join(''));
+};
+
+
+function renderPromotions(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        if (val.promotionable_type == "Store") {
+            var store_details = getStoreDetailsByID(val.promotionable_id);
+            val.store_detail_btn = store_details.slug ;
+            val.store_name = store_details.name;
+            val.image_url = store_details.store_front_url_abs;
+        }
+        else{
+            val.store_name = "Londonderry Mall";
+        }
+        
+        var show_date = moment(val.show_on_web_date);
+        var start = moment(val.start_date).tz(getPropertyTimeZone());
+        var end = moment(val.end_date).tz(getPropertyTimeZone());
+        if (start.format("DMY") == end.format("DMY")){
+            val.dates = start.format("YYYY-MM-DD")
+        }
+        else{
+            val.dates = start.format("YYYY-MM-DD") + " to " + end.format("YYYY-MM-DD")
+        }
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
+function renderPromotionDetails(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    item_list.push(collection)
+    $.each( item_list , function( key, val ) {
+        if (val.promotionable_type == "Store") {
+            var store_details = getStoreDetailsByID(val.promotionable_id);
+            val.store_detail_btn = store_details.slug ;
+            val.store_name = store_details.name;
+            val.image_url = store_details.store_front_url_abs;
+        }
+        else{
+            val.store_name = "Londonderry Mall";
+        }
+        
+        
+        var show_date = moment(val.show_on_web_date);
+        var start = moment(val.start_date).tz(getPropertyTimeZone());
+        var end = moment(val.end_date).tz(getPropertyTimeZone());
+        if (start.format("DMY") == end.format("DMY")){
+            val.dates = start.format("YYYY-MM-DD")
+        }
+        else{
+            val.dates = start.format("YYYY-MM-DD") + " to " + end.format("YYYY-MM-DD")
+        }
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
+
+function renderJobs(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        if(val.jobable_type == "Store"){
+            val.store_name = getStoreDetailsByID(val.jobable_id).name;
+            val.store_slug = getStoreDetailsByID(val.jobable_id).slug;
+        }
+        else{
+            val.store_name = "Londonderry Mall";
+        }
+        var show_date = moment(val.show_on_web_date);
+        var start = moment(val.start_date).tz(getPropertyTimeZone());
+        var end = moment(val.end_date).tz(getPropertyTimeZone());
+        if (start.format("DMY") == end.format("DMY")){
+            val.dates = start.format("MMM D")
+        }
+        else{
+            val.dates = start.format("MMM D") + " - " + end.format("MMM D")
+        }
+        
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
+
+function renderPosts(blog_template, blog_block, post){
+
+    var item_list = [];
+    var item_rendered = [];
+    var blog_template_html = $(blog_template).html();
+    Mustache.parse(blog_template_html);   // optional, speeds up future uses
+
+    $.each( post , function( key, val ) {
+        
+        var publish_date = moment(val.publish_date).tz(getPropertyTimeZone());
+        var today = moment().tz(getPropertyTimeZone());
+        if (publish_date <= today){
+            item_list.push(val);
+        }
+        
+        // item_list.push(val);
+    });
+    item_list.sort(function(a, b){
+        if(a.publish_date > b.publish_date) return -1;
+        if(a.publish_date < b.publish_date) return 1;
+        return 0;
+    });
+    $.each( item_list , function( key, val ) {
+            var date_blog = moment(val.publish_date).tz(getPropertyTimeZone());
+            val.published_on = date_blog.format("YYYY-MM-DD")
+            var blog_rendered = Mustache.render(blog_template_html,val);
+            item_rendered.push(blog_rendered);
+    });
+    $(blog_block).html(item_rendered.join(''));
+}
+
+
+
+
+function in_my_time_zone(hour, format){
+    return hour.tz(getPropertyTimeZone()).format(format)
+}
+
+function get_day(id){
+    switch(id) {
+        case 0:
+            return ("Sunday");
+            break;
+        case 1:
+            return ("Monday");
+            break;
+        case 2:
+            return ("Tuesday");
+            break;
+        case 3:
+            return ("Wednesday");
+            break;
+        case 4:
+            return ("Thursday");
+            break;
+        case 5:
+            return ("Friday");
+            break;
+        case 6:
+            return ("Saturday");
+            break;
+    }
+}
+function renderGeneral(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        var repo_rendered = Mustache.render(template_html,val);
+        item_rendered.push(repo_rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
